740f11d17c03efb9ecc17143a0fd95a88900ad4f
Michael Johndok <dazakeen@gmail.com>
2012-10-11 21:35:57 +0800

    Add the theme here
    Hello new theme

---
+++ Hotel.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>vbecomputing</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="Michael Johndok">
+
+<!-- Le styles -->
+<link href="<?php bloginfo(stylesheet_directory); ?>/css/bootstrap.css" rel="stylesheet">
+<link href="<?php bloginfo(stylesheet_directory); ?>/style.css" rel="stylesheet">
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- Le fav and touch icons -->
+<link rel="shortcut icon" href="../assets/ico/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo(stylesheet_directory); ?>/img/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo(’template_url’); ?>/img/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo(stylesheet_directory); ?>/img/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="<?php bloginfo(stylesheet_directory); ?>/img/apple-touch-icon-57-precomposed.png">
+</head>
+
+<body>
+<div id = "body">
+  <div id = "header" class="row">
+      <div id = "logo" class="span4">
+        <img src="<?php bloginfo(stylesheet_directory); ?>/img/logo.png">
+        <p>The slogan goes here</p>
+      </div><!-- end logo -->
+      <div id = "header_right" class="">
+        <div id = "header_menu" class="">
+          <ul class="">
+            <li><a href = "#" >My Trip             |</a></li>
+            <li><a href = "#" >My Trip        |</a></li>
+            <li><a href = "#" >My Trip        |</a></li>
+            <li><a href = "#" >My Trip        |</a></li>
+          </ul>
+        </div><!-- header_menu -->
+        <div id = "header_social" class="">
+          <img src="<?php bloginfo(stylesheet_directory); ?>/img/social.png">
+        </div><!-- header_social -->
+      </div><!-- end header_right -->
+  </div><!-- end header -->
+
+  <div id = "nav" class="row">
+        <div class="menu">
+            <ul>
+              <li><a >Hotel</a></li>
+              <li><a >Transportation</a></li>
+              <li><a >More</a></li>
+            </ul>         
+        </div>
+    </div><!-- end nav -->
+
+  <div id="content" class="container_in">
+    <div id = "explain" class="row">
+          <h2>Book the best reviewed hotels in the Haiti</h2>
+          <p>We’ve hand selected hotels that are consistently rated the best</p>
+    </div><!-- end explain -->
+
+    <div id = "middle_content" class="row">
+      <div class="">
+        <div id = "middle_content_left" class="">
+          <div class="" >
+            <div class="form_mk">
+              <img src="<?php bloginfo(stylesheet_directory); ?>/img/form.png">
+            </div><!-- end form_mk -->
+          </div>
+          <div class=" why" >
+            <h3>Why book with us ?</h3>
+            <div class="list dotted one">
+                <strong>Trust the World's largest online travel company</strong>
+                <p>31 million travellers globally book with us.</p>
+            </div>
+            <div class="list dotted good">
+              <strong>Choice</strong>
+              <p>Access to more than 150,000 hotels</p>
+            </div>
+            <div class="list dotted earth">
+              <strong>All Asia</strong>
+              <p>AirAsia The Only Online Travel Agent selling Air Asia Flights &</p>
+            </div>
+            <div class="list dotted money">
+              <strong>Combine & Save</strong>
+              <p>Book flight + hotel together to save time and money with 'Build</p>
+            </div>
+            <div class="list best">
+              <strong>Best Price Guarantee</strong>
+              <p>If you find a lower hotel price on an Indian travel site, we wi</p>
+            </div>
+          </div><!-- end why -->
+        </div><!-- middle_content_left -->
+        <div id = "middle_content_right" class="">
+          <div class="bridge">
+            <img src="<?php bloginfo(stylesheet_directory); ?>/img/bridge.png">
+          </div><!-- end bridge -->
+          <div class="diandian">
+            <img src="<?php bloginfo(stylesheet_directory); ?>/img/diandian.png">
+          </div><!-- end diandian -->
+          <div class="hipmunk ">
+            <h3>What is Hipmunk ?</h3>
+            <img src="<?php bloginfo(stylesheet_directory); ?>/img/xiong.png">
+          </div><!-- end hipmunk -->
+        </div><!-- middle_content_right -->
+      </div>
+    </div><!-- end middle_content -->
+
+    <div id = "footer" class="row">
+      <p class = "link">Mini | Privacy Policy | Terms of condition </p>  
+      <p class = "copyright">Copyright (c) 2012 Hipmunk, Inc. Hipmunk is a trademark of Hipm</p>
+    </div><!-- end footer -->
+
+  </div><!-- end content -->
+</div><!-- end container -->
+
+
+
+
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/jquery.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-transition.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-alert.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-modal.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-dropdown.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-scrollspy.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-tab.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-tooltip.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-popover.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-button.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-collapse.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-carousel.js"></script>
+<script src="<?php bloginfo(stylesheet_directory); ?>/js/bootstrap-typeahead.js"></script>
+
+</body>
+</html>
